3b04644d0c7689d2fb3b903028903a4e5e4d96fc
Anton Sulaev <a.sulaev@corp.mail.ru>
2022-01-13 12:21:10 +0300

    names refactoring

--- run_tests.sh
+++ run_tests.sh
@@ -3,17 +3,17 @@
 set -exuo pipefail
 
 root=$PWD
-arr=$(find . | grep '99_hw$' | grep -v '4/99_hw$' | grep -v '10/99_hw$' | grep -v '99_hw/code' |  grep -v '9/99_hw')
+arr=$(find . | grep '99_hw$' | grep -v 'net2/99_hw$' | grep -v 'ci_cd/99_hw$' | grep -v '99_hw/code' |  grep -v 'conf_monitoring/99_hw')
 for i in $arr; do golangci-lint -c .golangci.yml run $i/...;done
 
-if [ -d "$root/9/99_hw/server" ] 
+if [ -d "$root/09_conf_monitoring/99_hw/server" ]
 then
-    cd $root/9/99_hw/server
+    cd $root/09_conf_monitoring/99_hw/server
     golangci-lint -c $root/.golangci.yml run ./...
 fi
 
-if [ -d "$root/4/99_hw/taskbotr" ] 
+if [ -d "$root/04_net2/99_hw/taskbotr" ]
 then
-    cd $root/4/99_hw/taskbot
+    cd $root/04_net2/99_hw/taskbot
     golangci-lint -c $root/.golangci.yml run --modules-download-mode=vendor ./...
 fi
